8fd4c0ab44ba8cc741f1a7e108339265df20a28f
박정현 <solbaram94@gmail.com>
2023-10-21 18:46:11 +0900

    init ex_01

---
+++ test/com/howtodoinjava/demo/lucene/document/LuceneReadIndexTest.java
@@ -0,0 +1,13 @@
+package com.howtodoinjava.demo.lucene.document;
+
+import org.junit.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class LuceneReadIndexTest {
+    @Test
+    public void callMain() throws Exception {
+        String[] args = null;
+        LuceneReadIndex.main(args); // 에러
+    }
+}
---
+++ test/com/howtodoinjava/demo/lucene/document/LuceneWriteIndexTest.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.demo.lucene.document;
+
+import org.junit.Test;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class LuceneWriteIndexTest {
+    @Test
+    public void callMain() throws Exception {
+        String[] args = null;
+        LuceneWriteIndex.main(args); // 에러
+    }
+
+}
