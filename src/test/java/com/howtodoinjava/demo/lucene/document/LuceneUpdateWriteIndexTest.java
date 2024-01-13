2036d267ebc53c4aee7f80e99de7475eb3d3fc8a
박정현 <solbaram94@gmail.com>
2024-01-13 15:00:56 +0900

    D1 commit

--- src/test/java/com/howtodoinjava/demo/lucene/document/LuceneUpdateWriteIndexTest.java
+++ src/test/java/com/howtodoinjava/demo/lucene/document/LuceneUpdateWriteIndexTest.java
@@ -8,7 +8,7 @@ public class LuceneUpdateWriteIndexTest {
     public void callUpdateWrite() throws Exception {
         LuceneUpdateWriteIndex luceneUpdateWriteIndex = new LuceneUpdateWriteIndex();
         luceneUpdateWriteIndex.LuceneUpdateWriteIndexInit();
-        // D1
+        // 123524234
     }
 
 }
